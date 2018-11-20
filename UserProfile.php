7127a473f0520b8c940b1b1ea692c6a67a37e5e6
jwb16142 <anna.mcmonagle.2016@uni.strath.ac.uk>
2018-11-20 14:10:13 +0000

    sign up page

---
+++ UserProfile.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial scale=1.0">
+    <title>Love At First Site</title>
+</head>
+<body>
+<p>
+
+<h1 style="text-align:center;" > user profile</h1>
+
+
+
+</p>
+</body>
