3043a93826136e703596d78bbb28d29231df0af5
jwb16142 <anna.mcmonagle.2016@uni.strath.ac.uk>
2018-11-20 14:00:26 +0000

    sign up page

--- homePage.php
+++ homePage.php
@@ -4,9 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial scale=1.0">
     <title>Love At First Site</title>
-    <link rel="stylesheet" type="text/css" href="css_selectors.css"/>
 </head>
-<body style="background-color:DarkSalmon">
+<body>
 <p>
 
 <h1 style="text-align:center;" > Love at First Site <3 </h1>
@@ -17,6 +16,7 @@
 
 <form action = "UserProfile.php" method="POST">
     <button type="submit"   name="logIn"> Log in </button><br/><br/>
+
 </form>
 
 <form action = "SignupPage.php" method="POST">
