a7341020b8ec12a342462ed9cdd45226fc3a1183
Gregor <gregor.mcintyre.2016@uni.strath.ac.uk>
2018-11-20 12:44:05 +0000

    Basic sign up page

---
+++ SignupPage.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<div>
+    <h1>Please create your user profile</h1>
+</div>
+
+<form action="InsertUserProfile.php"method="post">
+    <p>
+        First name: <input type="text" name="firstname"/> <br/>
+        Surname: <input type="text" name="surname"/> <br/>
+        Username: <input type="text" name="username"/> <br/>
+        Email: <input type="text" name="email"/> <br/>
+        Confirm Email: <input type="text" name="confirmemail"/> <br/>
+        Password: <input type="text" name="password"/> <br/>
+        Confirm Password: <input type="text" name="confirmpassword"/> <br/>
+        Age: <input type="text" name="age"/> <br/>
+        Location: <input type="text" name="location"/> <br/>
+        Short Bio: <input type="text" name="bio"/> <br/>
+
+        <input type="submit"/>
+    </p>
+</form>
+
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign up page</title>
+</head>
+<body>
+
+
+</body>
