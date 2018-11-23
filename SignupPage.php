2c48de2ba822e2bacbd47667a85ae51c99c4156e
Milda Petrikaite CS2018 <milda.petrikaite.2018@uni.strath.ac.uk>
2018-11-23 22:55:12 +0000

    Linked sign up page with database (inserting new entries) and added options to gender and country fields

--- SignupPage.php
+++ SignupPage.php
@@ -8,6 +8,9 @@
 <div class="content_wrapper">
     <?php
 
+    $genderOptions;
+    $countryOptions;
+
     // Connect to database.
     $host = "devweb2018.cis.strath.ac.uk";
     $user = "cs312groupq";
@@ -28,10 +31,33 @@
     $password =      strip_tags(isset ($_POST["password"])     ?$_POST["password"]:"");
     $confirmPassword = strip_tags(isset ($_POST["confirmPassword"])  ?$_POST["confirmPassword"]:"");
     $age =           strip_tags(isset ($_POST["age"])          ?$_POST["age"]:"");
+    $gender =        strip_tags(isset ($_POST["gender"])       ?$_POST["gender"]:"");
     $location =      strip_tags(isset ($_POST["location"])     ?$_POST["location"]:"");
 
-    if (($name==="") || ($username==="") || (!filter_var($email, FILTER_VALIDATE_EMAIL)) || ($password==="") || ($age<18) || ($location==="") || ($email != $confirmEmail)
-    || ($password != $confirmPassword)) {
+    $sql = "SELECT * FROM `genders`";
+    $result = $conn->query($sql);
+
+    if($result->num_rows > 0){
+        $i = 0;
+        while($row = $result->fetch_assoc()){
+            $genderOptions[$i] = $row;
+            $i++;
+        }
+    }
+
+    $sql = "SELECT * FROM `countries`";
+    $result = $conn->query($sql);
+
+    if($result->num_rows > 0){
+        $i = 0;
+        while($row = $result->fetch_assoc()){
+            $countryOptions[$i] = $row;
+            $i++;
+        }
+    }
+
+    if (($name==="") || ($username==="") || (!filter_var($email, FILTER_VALIDATE_EMAIL)) || ($password==="") || ($age==="") || ($location==="") || ($email != $confirmEmail)
+    || ($password != $confirmPassword) || ($gender==="")) {
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
         echo "<p> Please complete all fields.</p>";
     }
@@ -49,20 +75,45 @@
                     <tr><td><label>Password:</label></td> <td><input type="password" name="password" value="<?php echo $password; ?>"/> </td></tr>
                     <tr><td><label>Confirm Password:</label></td> <td><input type="password" name="confirmPassword" value="<?php echo $confirmPassword; ?>"/> </td></tr>
                     <tr><td><label>Age:</label></td> <td><input type="text" name="age" value="<?php echo $age; ?>"/> </td></tr>
-                    <tr><td><label>Location:</label></td> <td><input type="text" name="location" value="<?php echo $location; ?>"/> </td></tr>
+                    <tr><td><label>Gender:</label></td> <td> <select name="gender">
+                                <option value="" selected disabled value>select gender<br>
+                                    <?php
+                                    foreach ($genderOptions as $g){
+                                        echo "<option value=\"".$g['id']."\"";
+                                        if($g['id']===$gender) echo " selected=\"selected\"";
+                                        echo ">".$g['name']."</option><br>";
+                                    }
+                                    echo "</select><br>";
+                                    ?>
+                    </td></tr>
+                    <tr><td><label>Location:</label></td> <td> <select name="location">
+                                <option value="" selected disabled value>select country<br>
+                                    <?php
+                                    foreach ($countryOptions as $c){
+                                        echo "<option value=\"".$c['id']."\"";
+                                        if($c['id']===$location) echo " selected=\"selected\"";
+                                        echo ">".$c['name']."</option><br>";
+                                    }
+                                    echo "</select><br>";
+                                    ?>
+                    </td></tr>
                 </table>
                 <div class="buttons"><button type="submit">Submit</button></div>
             </form>
         </div>
                 <?php
             }
-            $msg = "Thank you for signing up to Love At First Site! Please click the link to access your account.. https://devweb2018.cis.strath.ac.uk/~jwb16142/312/groupq/UserProfile.php " ;
-            mail($email, "Love at First Site", $msg);
 
-            echo  "Hello ".$name." and Welcome to Love at First Site!";
-            echo  " Are you ready to find your soul mate? " ;
-            echo  "Click on the link sent to your email address ".$email;
+            $sql = "INSERT INTO `user_profiles`(`name`, `username`, `email`, `password`, `age`, `gender`, `location`) VALUES (\"$name\",\"$username\", \"$email\", \"$password\", \"$age\", \"$gender\", \"$location\")";
 
+            if($conn->query($sql) === TRUE){
+                $msg = "Thank you for signing up to Love At First Site! Please click the link to access your account.. https://devweb2018.cis.strath.ac.uk/~jwb16142/312/groupq/UserProfile.php " ;
+                mail($email, "Love at First Site", $msg);
+
+                echo  "Hello, ".$name.", and Welcome to Love at First Site!\n";
+                echo  "Are you ready to find your soul mate?\n" ;
+                echo  "Click on the link sent to your email address ".$email."\n";
+            }
             ?>
         </div>
 </div>
