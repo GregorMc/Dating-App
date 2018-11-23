710dd71c504ae0d4553b0d78b532527a662f384a
Rokas Aidukas <djb16170@strath.ac.uk>
2018-11-23 17:41:35 +0000

    Set up proper deployment settings and started on some cleanup.

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial scale=1.0">
+    <title>Love at First Site</title>
+
+</head>
+<body>
+<p>
+
+<h1 style="text-align:center;" > Love at First Site </h1>
+<link rel="stylesheet" type="text/css" href="Design.css"/>
+<div class="main_form">
+    <label>Username :</label> <input type="text" name="userName"/> <br>
+    <label>Password :</label> <input type="text" name="password"/> <br>
+    <div class="buttons">
+        <button formaction="UserProfile.php" formmethod="post" type="submit" name="logIn"> Log in </button>
+        <button formaction="SignupPage.html" formmethod="post" type="submit" name="signUp"> Sign up </button>
+    </div>
+</div>
+</body>
+
+<?php
+
+
+
+
+
+
+?>
