387a4f5286e033d0f0a24239ceef9cb3cea9fc93
Milda Petrikaite CS2018 <milda.petrikaite.2018@uni.strath.ac.uk>
2018-11-26 02:35:06 +0000

    edit profile update

--- editProfile.php
+++ editProfile.php
@@ -1,15 +1,11 @@
 <?php
 include "connectDB.php";
-?>
-    <!DOCTYPE html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial scale=1.0">
-        <link rel="stylesheet" type="text/css" href="Design.css"/>
-        <title>Love At First Site</title>
-    </head>
 
-<?php
+session_start();
+
+if(!isset($_SESSION['id'])){
+    header('Location: https://devweb2018.cis.strath.ac.uk/cs312groupq/index.php');
+}
 
 function test_input($test_data) {
     $test_data = trim($test_data);
@@ -21,7 +17,6 @@ function test_input($test_data) {
 
 $genderOptions;
 $countryOptions;
-$prompt = "";
 
 $sql = "SELECT * FROM `genders`";
 $genderOptions = connectDB::select($sql);
@@ -29,13 +24,10 @@ $genderOptions = connectDB::select($sql);
 $sql = "SELECT * FROM `countries`";
 $countryOptions = connectDB::select($sql);
 
-$id;
+$id = $_SESSION['id'];
 
 $name = $age = $gender = $location = "";
-//reikia hidden id fieldo
-if(!isset($_POST["id"])){
-    $id = "3";
-
+if(!isset($_POST["name"])){
     $sql = "SELECT * FROM `user_profiles` WHERE `id`=\"$id\"";
     $result = connectDB::select($sql);
 
@@ -47,15 +39,19 @@ if(!isset($_POST["id"])){
     }
 }
 else{
-    $id = isset($_POST["id"])?$_POST["id"]:"";
-
     $name =          test_input(isset ($_POST["name"])         ?$_POST["name"]:"");
     $age =           test_input(isset ($_POST["age"])          ?$_POST["age"]:"");
     $gender =        test_input(isset ($_POST["gender"])       ?$_POST["gender"]:"");
     $location =      test_input(isset ($_POST["location"])     ?$_POST["location"]:"");
 }
 ?>
-
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial scale=1.0">
+    <link rel="stylesheet" type="text/css" href="Design.css"/>
+    <title>Love At First Site</title>
+</head>
     <body>
     <div class="content_wrapper">
         <div class="header">
@@ -63,18 +59,11 @@ else{
         </div>
         <?php
 
-        if (($name==="") || ($age==="") || ($location==="")|| ($gender==="") || !isset($_POST["first"])) {
-            if ($_SERVER["REQUEST_METHOD"] === "POST") {
-                $prompt = "Please complete all fields.";
-            }
-
             ?>
             <h1>Edit profile</h1>
             <div class="main_form">
-                <?php echo "<span class='error_span'>$prompt</span>";?>
-                <form action="editPage.php" method="post">
+                <form action="editProfile.php" method="post">
                     <table>
-                        <label><input type="text" name="id" value="<?php echo $id; ?>" hidden/></label>
                         <label><input type="text" name="first" value="false" hidden/></label>
                         <tr><td><label>Name:</label></td> <td><input type="text" name="name" value="<?php echo $name; ?>"/> </td></tr>
                         <tr><td><label>Age:</label></td> <td><input type="text" name="age" value="<?php echo $age; ?>"/> </td></tr>
@@ -103,21 +92,21 @@ else{
                     </table>
                     <div class="buttons">
                         <button formaction="UserProfile.php">Back</button>
-                        <button type="submit">Submit</button>
+                        <button type="submit" name="edit" value="aaa">Submit</button>
                     </div>
                 </form>
             </div>
             <?php
-        }
-        else{
+
+
             $sql = "UPDATE `user_profiles` SET  `name`=\"$name\", `age`=\"$age\", `gender`=\"$gender\", `location`=\"$location\" WHERE `id`=\"$id\"";
 
-            if(connectDB::query($sql) === TRUE){
+            if(connectDB::query($sql) === TRUE && !empty($_POST["edit"])){
                 echo  "updated";
             }
 
             connectDB::disconnect();
-        }
+
         ?>
     </div>
     </div>
